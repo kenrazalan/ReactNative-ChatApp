b73e89d6b67a313bfcde949a3b76b5e5e2a42056
kenrazalan <kennethrazalan29@gmail.com>
2021-05-30 11:53:24 +0800

    add register screen

---
+++ screens/Register.js
@@ -0,0 +1,77 @@
+import React, { useLayoutEffect, useState } from 'react'
+import { KeyboardAvoidingView } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import {StatusBar} from 'expo-status-bar'
+import {Button,Input,Text} from 'react-native-elements'
+
+const Register = ({navigation}) => {
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [image,setImage] = useState('')
+
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerBackTitle: "Login"
+        })
+    },[navigation])
+
+    const register = () =>{
+
+    }
+    return (
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
+
+            <Text h3 >Create Accoun</Text>
+        <View style={styles.input}>
+            <Input 
+                placeholder="Full Name" 
+                
+                type="text"
+                value={name}
+                onChangeText={text => setName(text)}/>
+            <Input 
+                placeholder="Email" 
+                type="email"
+                value={email}
+                onChangeText={text => setEmail(text)}/>
+            <Input 
+                secureTextEntry
+                placeholder="Password" 
+                type="password"
+                value={password}
+                onChangeText={text => setPassword(text)}/>
+            <Input 
+                placeholder="Profile Picture" 
+                type="text"
+                value={image}
+                onChangeText={text => setImage(text)}
+                onSubmitEditing={register}/>
+        </View>
+        <Button 
+            containerStyle={styles.button}
+            raised
+            title="Register"
+            onPress={register}/>
+        </KeyboardAvoidingView>
+    )
+}
+
+export default Register
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems:"center",
+        justifyContent: "center",
+
+    },
+    input: {
+        width: 300
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
+
+    }
+})
