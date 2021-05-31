39152a756899897f99ef2a91781756e0fed35c1a
kenrazalan <kennethrazalan29@gmail.com>
2021-05-31 16:25:54 +0800

    render all list item

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,26 @@
-import React, { useLayoutEffect } from 'react'
-import { SafeAreaView,ScrollView } from 'react-native'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { SafeAreaView,ScrollView,TouchableOpacity } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import { Avatar } from 'react-native-elements/dist/avatar/Avatar'
 import CustomListItem from '../components/CustomListItem'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
+import { AntDesign,SimpleLineIcons } from '@expo/vector-icons';
 
 
 const Home = ({navigation}) => {
+    const [chats,setChats] = useState([])
+
+    useEffect(()=>{
+        const unsubscribe = db.collection('chats').onSnapshot(snapshot =>{
+            setChats(snapshot.docs.map(doc => (
+                {
+                    id: doc.id,
+                    data: doc.data()
+                }
+        )))
+        })
+        return unsubscribe
+    },[])
 
     useLayoutEffect(()=>{
         navigation.setOptions({
@@ -14,17 +28,43 @@ const Home = ({navigation}) => {
             headerStyle:{ backgroundColor: "white" },
             headerTitleStyle: {color: "black"},
             headerTintColor: {color: "black"},
-            headerLeft: () =>{ <View style={{marginLeft: 20}}>
+            headerLeft: () =>(
+            <View style={styles.headerLeftIcon}>
+                <TouchableOpacity onPress={signOut}>
                 <Avatar rounded source={{ 
                     uri: auth?.currentUser?.photoURL
                 }}/>
-            </View>}
+                </TouchableOpacity>
+            </View>),
+            headerRight: () =>(
+            <View style={styles.headerRightIcon}>
+                <TouchableOpacity activeOpacity={0.5}>
+                    <AntDesign name="camerao" size={24} color="black"/>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    activeOpacity={0.5}
+                    onPress={()=>{
+                        navigation.navigate("NewChat")
+                    }}>
+                    <SimpleLineIcons name="pencil" size={24} color="black"/>
+                </TouchableOpacity>
+            </View>),
+
         })
     },[navigation])
+
+    const signOut = ()=>{
+        auth.signOut().then(()=>{
+            navigation.replace("Login")
+        })
+    }
+   
     return (
         <SafeAreaView>
-            <ScrollView>
-                <CustomListItem  />
+            <ScrollView style={styles.container}>
+                {chats.map(({id,data: {chatName}})=>(
+                    <CustomListItem key={id} id={id} chatName={chatName} />
+               ) )}
             </ScrollView>
         </SafeAreaView>
     )
@@ -32,4 +72,17 @@ const Home = ({navigation}) => {
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container :{
+        height: "100%"
+    },
+    headerLeftIcon:{
+        marginLeft: 20
+    },
+    headerRightIcon: {
+        flexDirection:"row",
+        marginRight: 20,
+        width: 80,
+        justifyContent: 'space-between'
+    }
+})
