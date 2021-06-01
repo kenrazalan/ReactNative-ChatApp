b5459049e22fcf660500d8bde67b68c7d095a483
kenrazalan <kennethrazalan29@gmail.com>
2021-06-01 21:23:19 +0800

    add last message to list item

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,24 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import {Avatar,ListItem} from 'react-native-elements'
+import { db } from '../firebase'
 
 
 const CustomListItem = ({id,chatName,openChat}) => {
+    const [messages,setMessages] = useState([])
+
+    useEffect(()=>{
+        const unsubscribe = db.collection('chats')
+        .doc(id).collection('messages')
+        .orderBy('timestamp','asc')
+        .onSnapshot(snapshot => 
+            setMessages(snapshot.docs.map(doc => doc.data())))
+        return unsubscribe
+    },[])
     return (
-        <ListItem onPress={()=>{
+        <ListItem key={id} onPress={()=>{
             openChat(id,chatName)
         }} key={id} bottomDivider>
             <Avatar 
             rounded
             source={{
-                uri:"https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png"
+                uri: messages?.[messages.length -1]?.image || 
+                "https://p.kindpng.com/picc/s/78-785827_user-profile-avatar-login-account-male-user-icon.png"
             }} />
         <ListItem.Content>
             <ListItem.Title style={{fontWeight:"bold"}}>
                 {chatName}
             </ListItem.Title>
             <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                Subtitle
+               <Text style={{fontWeight: "bold"}}>
+                   {messages?.[messages.length -1]?.displayName}
+              </Text>: {messages?.[messages.length -1]?.message}
             </ListItem.Subtitle>
         </ListItem.Content>
         </ListItem>
